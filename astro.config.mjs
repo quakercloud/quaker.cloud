824dd7d5dc388708f5f911ddf00f4678b5479060
Brylie Christopher Oxley <brylie@protonmail.com>
2026-01-06 19:40:18 +0200

    Add sitemap integration to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Quaker Cloud',
-      social: [ { icon: 'github', label: 'GitHub', href: 'https://github.com/quakercloud/quaker.cloud' } ],
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: 'Quaker Cloud',
+    social: [ { icon: 'github', label: 'GitHub', href: 'https://github.com/quakercloud/quaker.cloud' } ],
+    sidebar: [
+      {
+        label: 'Guides',
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: 'Example Guide', slug: 'guides/example' },
+        ],
+      },
+      {
+        label: 'Reference',
+        autogenerate: { directory: 'reference' },
+      },
+    ],
+  }), sitemap()],
 });
